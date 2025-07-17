33110400e369945e7a98dd5e7b958d91605c4a44
felxx <igorfelipemonteirocontato@gmail.com>
2025-07-17 13:55:01 -0300

    feat: implements card microservice configuration

---
+++ card-microservice/src/main/java/com/felxx/card_microservice/CardMicroserviceApplication.java
@@ -0,0 +1,12 @@
+package com.felxx.card_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardMicroserviceApplication.class, args);
+	}
+}
---
+++ card-microservice/src/test/java/com/felxx/card_microservice/CardMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felxx.card_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
