07789b6d8521fdf53f324c7b2b715d039f6e88e3
felxx <igorfelipemonteirocontato@gmail.com>
2025-07-17 13:44:33 -0300

    feat: implements cloud gateway on the project

---
+++ cloud-gateway/src/main/java/com/felxx/cloud_gateway/CloudGatewayApplication.java
@@ -0,0 +1,24 @@
+package com.felxx.cloud_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder) {
+		return builder.routes()
+		.route(r -> r.path("/clients/**").uri("lb://client-microservice"))
+		.build();
+	}
+}
